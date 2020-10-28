256768df9ddc346531d6b2c7e85f01407147f09f
greensleeves8 <theonlycalegreen@gmail.com>
2020-10-28 03:32:38 -0500

    Module Challenge 1 & 2

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -23,3 +23,16 @@ INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT e.emp_no,
+    e.first_name,
+    e.last_name,
+    ti.title,
+    ti.from_date,
+    ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti
+ON e.emp_no = ti.emp_no
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no; 
